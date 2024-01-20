9733cd48cde244ad876778d6a1c6c5af61e023fa
Molly Su <mollydcxxiii@gmail.com>
2024-01-20 10:56:37 +0800

    Update HomeNavBar.jsx

--- src/components/NavBar/HomeNavBar.jsx
+++ src/components/NavBar/HomeNavBar.jsx
@@ -1,27 +1,25 @@
-const HomeNavBar = () => {
-	return (
-		<header className='flex justify-between py-4 text-zinc-600 items-center'>
-			<div className='space-x-5'>
-				<a href='#' className='hover:underline cursor-pointer'>
-					Write a Review
-				</a>
-				<a href='#' className='hover:underline cursor-pointer'>
-					Events
-				</a>
-			</div>
-			<div className='space-x-2 md:space-x-4'>
-				<button type='button' className='px-2.5 hover:underline cursor-pointer'>
-					Log In
-				</button>
-				<button
-					type='button'
-					className='bg-white focus:outline-none border active:bg-zinc-50 py-0.5 px-2.5 rounded border-zinc-300 hover:shadow-inner'
-				>
-					Sign up
-				</button>
-			</div>
-		</header>
-	)
-}
+const HomeNavBar = () => (
+	<header className='flex justify-between py-4 text-zinc-600 items-center'>
+		<div className='space-x-5'>
+			<a href='#' className='hover:underline cursor-pointer'>
+				Write a Review
+			</a>
+			<a href='#' className='hover:underline cursor-pointer'>
+				Events
+			</a>
+		</div>
+		<div className='space-x-2 md:space-x-4'>
+			<button type='button' className='px-2.5 hover:underline cursor-pointer'>
+				Log In
+			</button>
+			<button
+				type='button'
+				className='bg-white focus:outline-none border active:bg-zinc-50 py-0.5 px-2.5 rounded border-zinc-300 hover:shadow-inner'
+			>
+				Sign up
+			</button>
+		</div>
+	</header>
+)
 
 export default HomeNavBar
